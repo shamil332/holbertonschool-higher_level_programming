5beccf1f86624eb659275004f0aacc34b6890669
shamil332 <shamilkazimli2006@gmail.com>
2025-11-18 06:23:27 +0000

    task2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        for i in range(len(argv)):
+            print("{}: {}".format(i, argv[i]))
