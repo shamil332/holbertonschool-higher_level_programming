2557d32b97f4d720873e671cf5847a91cce1a762
shamil332 <shamilkazimli2006@gmail.com>
2025-11-18 06:26:23 +0000

    small update

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(len(argv)))
+        print("{} arguments:".format(len(argv)-1))
         for i in range(1, len(argv)):
             print("{}: {}".format(i, argv[i]))
