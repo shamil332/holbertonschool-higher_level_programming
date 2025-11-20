591945b829e53977c61c43b02ead86ba222f8ed4
shamil332 <shamilkazimli2006@gmail.com>
2025-11-20 08:21:01 +0000

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,7 +9,7 @@ def roman_to_int(roman_string):
         "D": 500,
         "M": 1000
     }
-    if not isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     total = 0
     for i in range(len(roman_string)):
