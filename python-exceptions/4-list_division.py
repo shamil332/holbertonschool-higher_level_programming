9fb77f76a6f9e2ebb8d2058fe47ac8ec5a7f1999
shamil332 <shamilkazimli2006@gmail.com>
2025-11-21 06:49:56 +0000

    fix

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -12,7 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 try:
                     val = a / b
                 except ZeroDivisionError:
-                    print("Division by 0")
+                    print("division by 0")
         except IndexError:
             print("out of range")
         finally:
