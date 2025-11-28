9d6cdd42dbf12aaead8551bb5820d3ddb56a176d
shamil332 <shamilkazimli2006@gmail.com>
2025-11-28 08:05:21 +0000

    small correction

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,6 +4,7 @@ Module that defines is_kind_of_class.
 Checks if an object is an instance of a class or its subclasses.
 """
 
+
 def is_kind_of_class(obj, a_class):
     """Return True if obj is an instance of a_class or its subclasses."""
     return isinstance(obj, a_class)
