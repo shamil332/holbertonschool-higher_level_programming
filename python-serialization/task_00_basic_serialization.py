2b5015ab63debc4b0035d961f171882d5a7c8872
shamil332 <shamilkazimli2006@gmail.com>
2025-11-28 19:13:25 +0000

    task 0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,7 +6,7 @@ import json
 def serialize_and_save_to_file(data, filename):
     # Your code here to serialize and save data to the specified file
     with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(data, f, default=str)
+        json.dump(data, f)
 
 def load_and_deserialize(filename):
     # Your code here to load and deserialize data from the specified file
