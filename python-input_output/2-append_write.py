a76156f291d9c3981c72238eb67a8a81b00fa858
shamil332 <shamilkazimli2006@gmail.com>
2025-11-28 17:32:16 +0000

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF-8 text file and returns number of characters added."""
+    """Appends a string to the end of a UTF-8 text file"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
