031e49ace6868131804cf88a838480a02999a9e6
shamil332 <shamilkazimli2006@gmail.com>
2025-11-28 17:26:10 +0000

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""module for task 1"""
+
+
 def write_file(filename="", text=""):
+    """writes to a file"""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
