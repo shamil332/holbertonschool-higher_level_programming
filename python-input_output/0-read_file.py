cceac6dc95427ab9ad814402bf0cb9a71f61b5dc
shamil332 <shamilkazimli2006@gmail.com>
2025-11-28 17:19:52 +0000

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""module for task 1"""
+
+
 def read_file(filename=""):
+    """Reads a UTF-8 text file and prints it to stdout."""
     with open('filename', 'r', encoding="utf-8") as f:
         print(f.read(), end="")
