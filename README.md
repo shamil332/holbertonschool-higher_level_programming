cae90a76c947d07e0bf8999ff8519b7faeeae9f2
shamil332 <shamilkazimli2006@gmail.com>
2025-11-28 19:09:07 +0000

    task 0

--- README.md
+++
@@ -1,1 +0,0 @@
-this repo is for the python projects
---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Basic serialization module: save/load Python dictionaries as JSON"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    # Your code here to serialize and save data to the specified file
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    # Your code here to load and deserialize data from the specified file
+    with open(filename, 'r', encoding='utf-8') as f:
+        json.load(filename)
