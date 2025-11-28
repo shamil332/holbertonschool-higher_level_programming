0aced46441c625d5d0b0aadcac3026af796bf4e5
shamil332 <shamilkazimli2006@gmail.com>
2025-11-28 17:50:31 +0000

    task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module for the task 5"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w", encoding="utf-8") as f:
+        f.dump(my_obj, f)
