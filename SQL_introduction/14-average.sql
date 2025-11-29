0e37665abdbf5e40337fee9bb9ffd3ba18234e2a
shamil332 <shamilkazimli2006@gmail.com>
2025-11-29 18:48:40 +0000

    task 14

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- a script that computes the score average of all records in the table second_table of the database hbtn_0c_0.
-SELECT avg(*) as average
+SELECT AVG(*) as average
 FROM second_table;
