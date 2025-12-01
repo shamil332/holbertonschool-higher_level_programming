1c5f1a687ad970b86bfe92055664609af538ee90
shamil332 <shamilkazimli2006@gmail.com>
2025-12-01 19:16:18 +0000

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status using urllib"""
 import urllib.request
 
 if __name__ == "__main__":
-    url  = "https://intranet.hbtn.io/status"
+    url = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
