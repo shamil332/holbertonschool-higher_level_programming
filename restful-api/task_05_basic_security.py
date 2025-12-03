07000feccba9b669357674197680a10a4655e787
shamil332 <shamilkazimli2006@gmail.com>
2025-12-03 18:58:47 +0000

    small correction

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import (
     JWTManager, create_access_token,
     jwt_required, get_jwt_identity
 )
+from flask import request, jsonify
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
