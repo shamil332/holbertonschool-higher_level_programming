6bfdc9e47b4e875d7b6cf261681048047dcc452a
skazimli <skazimli20092@ada.edu.az>
2025-12-09 09:16:46 +0400

    advanced task

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 	user = sys.argv[2]
 	url = f"https://api.github.com/repos/{owner}/{repo}/commits"
 	params = {"per_page": 10}
-	r = requests.get(url, params=params}
+	r = requests.get(url, params=params)
 	commits = r.json()
 	for commit in commits:
 		sha = commit.get("sha")
