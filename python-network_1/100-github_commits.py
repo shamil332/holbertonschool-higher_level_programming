7af4fd86df687515c1886cccaaed3528e9fd4a6a
skazimli <skazimli20092@ada.edu.az>
2025-12-09 09:22:25 +0400

    last changes

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-this script lists 10 commits from the repo
-"rails" by the user "rails".
+this script list 10 commits from
+the repo "rails" by the user "rails"
 """
 import sys
 import requests
 
+
 if __name__ == "__main__":
-	repo = sys.argv[1]
-	user = sys.argv[2]
-	url = f"https://api.github.com/repos/{user}/{repo}/commits"
-	params = {"per_page": 10}
-	r = requests.get(url, params=params)
-	commits = r.json()
-	for commit in commits:
-		sha = commit.get("sha")
-		auth = commit.get("commit", {}).get("author", {}).get("name")
-		print(f"{sha}: {auth}")
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    params = {"per_page": 10}
+
+    r = requests.get(url, params=params)
+    commits = r.json()
+
+    for commit in commits:
+        sha = commit.get("sha")
+        author = commit.get("commit", {}).get("author", {}).get("name")
+        print(f"{sha}: {author}")
